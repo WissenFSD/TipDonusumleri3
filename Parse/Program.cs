8267dfea678a00e863285d99550e1a6bc8f22178
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-18 21:28:02 +0300

    Add project files.

---
+++ Parse/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// ToString her şeyi string'ê çevirmek için kullanılır
+// Parse ise her şeyi stringden çevirmek için kullanır
+
+
+// string tipindeki 10 değerini sayısal değer olan int tipine parse ile çevirdik 
+string sayi = "10";
+int intSayi = int.Parse(sayi);
+
+
+// string tipini btye tipine çevirme
+
+string stringSayi = "50";
+byte byteSayi = byte.Parse(stringSayi);
+
+
+#region Örnek
+// Kullanıcıdan string değer alıp, int bir diziyi dolduran bir örnek yapalım(Parse ile)
+
+//Console.WriteLine("Değer gir");
+//int deger =int.Parse( Console.ReadLine());
+
+//int[] dizi = new int[deger];
+//int toplam = 0;
+//for (int i = 0; i < deger; i++)
+//{
+//    Console.WriteLine("{0}. sayıyı gir",i);
+//    dizi[i]=int.Parse(Console.ReadLine());
+//    toplam += dizi[i];
+//}
+//Console.WriteLine("Veriler alındı. Devam etmek için tuşa bas");
+//Console.ReadLine();
+//Console.WriteLine("Girilen Sayı Adedi   {0} -- Toplam : {1} --- Ortalama : {2}",deger,toplam,toplam/deger);
+#endregion
+
+#region Örnek 2
+
+// girilen tüm sayıların benzersiz olup olmadığını kontrol ediniz kod örneğini yazınız
+// Ev Ödevi
+
---
+++ TipDonusumleri(CAST)/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// ToString
+// Convert
+// Parse
+// Cast
+
+
+// küçük tip büyük tip'ê çevrildiğinde bir veri kaybı olmayacağı için her hangi bir tip dönüşümü yapmamıza gerek kalmıyor
+
+// küçük tip büyük tipe atandı
+//byte a = 10;
+//int b = a;
+
+// daha büyük olanın daha küçük olan tipe dönüşümünde gerçekleşlir(Bu senaryo da bilinçli bir şekilde tip dönüşümünü onaylamamız gerekiyor)
+//int buyukTip = 10;
+//byte kucukTip = (byte)buyukTip;
+
+
+// Ondalık sayılar
+// Ondalik sayı tam sayıya çevrildiğinde ondalık kısmı yok olacaktır. Bu tip dönüşümüne dikkat etmelisiniz.
+//double ondalik = 10.6;
+//int ondalikOlmayan = (int)ondalik;
+//Console.WriteLine(ondalikOlmayan);
+
+
+// Cast yöntemi ile string char'a dönüştürülemez
+//string degisken = "Merhaba";
+//char tekKarakter = (char)degisken;
+
+// Bu şekilde de yöntem değildir.
+//string tekKarakterString = "A";
+//char tekKarakterChar = (char)tekKarakterString;
+
+
+// Büyük değer taşıyab değişken , küçük değer taşıyan değişkene atandığı zaman, veri kaybı olacaktır.
+//int sayi = 260;
+//byte kucukSayi = (byte)sayi;
+//Console.WriteLine(kucukSayi);
+
+
+// Char'da string ifadesine dönüştürülemez(ToString kullanılır.)
+char tekKarakter = 'a';
+string cokKarakter = tekKarakter.ToString();
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
